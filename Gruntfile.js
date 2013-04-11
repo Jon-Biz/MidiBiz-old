b58d4cf7a77739a3d7bfa9dcc0fbc9ff731de5a7
Jon <srndpty@gmail.com>
2013-04-11 14:50:53 -0700

    added grunt js

---
+++ Gruntfile.js
@@ -0,0 +1,100 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    meta: {
+      version: '0.1.0'
+    },
+    banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '* http://PROJECT_WEBSITE/\n' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+      'YOUR_NAME; Licensed MIT */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['app/**/*.js'],
+        dest: 'dist/FILE_NAME.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/FILE_NAME.min.js'
+      }
+    },
+/*    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        laxcomma:true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['app/*.js']
+      }
+    },
+    */
+    jasmine:{
+      src:['app/modules.vies.js'],
+      options:{
+        vendor:[
+           'libs/js/json2.js',
+          'libs/js/jquery-1.8.3.js',
+          'libs/js/underscore.js',
+          'libs/js/backbone.js',
+          'libs/js/backbone.marionette.js',
+          'libs/js/Bacon.js',
+          'jasmine/lib/*.js'
+          ],
+         specs:['jasmine/spec/**/sandboxSpec.js','modelSpec.js']
+        }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task.
+  grunt.registerTask('default', ['jasmine', 'concat', 'uglify']);
+
+};
