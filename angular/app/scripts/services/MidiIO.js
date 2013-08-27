d8b304277f71f933ec6b1509c5c849f81ef5d55f
Jon <srndpty@gmail.com>
2013-08-27 12:50:46 -0700

    added actual midi output to midiout objects 	modified:   scripts/services/MidiIO.js

--- angular/app/scripts/services/MidiIO.js
+++ angular/app/scripts/services/MidiIO.js
@@ -42,11 +42,25 @@ MidiIO.factory('midiservice',function ($timeout) {
 		});
 
 		_.each(MIDIAccess.enumerateOutputs(),function(device,index){
-			var id = 'output-'+replacespace(device.deviceName);
-			update_outputs({
-				'id':id,
-				'name':device.deviceName
-			});
+			if(device.deviceName != "Microsoft GS Wavetable Synth"){
+
+
+				var id = 'output-'+replacespace(device.deviceName);
+
+				var	midiout = {
+					'id':id,
+					'name':device.deviceName,
+					'output': MIDIAccess.getOutput(device)
+				}
+
+				var midiMessage = MIDIAccess.createMIDIMessage(midiBridge.NOTE_ON, 1, 48, 100);
+				midiout.output.sendMIDIMessage(midiMessage);
+
+				update_outputs(midiout);
+
+			}
+
+
 		});
 
 //		devices.innerHTML += "<div>Output length is "+MIDIAccess.enumerateOutputs().length+"</div>";
