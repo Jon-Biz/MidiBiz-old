96fb5c6e31d84dc01d5230a8c4c861a00517c63f
Jon <srndpty@gmail.com>
2013-09-07 21:53:35 -0700

    added IO factory + tests

--- app/scripts/services/Puts.service.js
+++ app/scripts/services/Puts.service.js
@@ -8,4 +8,27 @@ angular.module('Puts',[])
 			outputs : []
 		};
 		return puts;
+	})
+	.service('IOFactory',function($timeout){
+		return{
+			getInputs:function(){
+				this.inputs = [];
+				this.outputs = [];
+
+				this.addInput = function (input) {
+					var inputs = this.inputs;
+					$timeout(function () {
+						inputs.push(input);
+					},100);
+				};
+
+				this.addOutput = function(output) {
+					var outputs = this.outputs;
+					$timeout(function () {
+						outputs.push(output);
+					},100);
+				};
+
+			}
+		};
 	});
