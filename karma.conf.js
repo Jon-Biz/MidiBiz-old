c2e0cfd819a5eeb6edca8b6a7a4e9fbf8b1721ca
Jon <srndpty@gmail.com>
2013-09-07 19:11:33 -0700

    updated library refs

--- karma.conf.js
+++ karma.conf.js
@@ -11,12 +11,17 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'app/bower_components/bacon/dist/Bacon.js',
+      'app/bower_components/jquery/jquery.js',
+      'app/bower_components/jquery.ui/dist/jquery-ui.js',
+      'app/bower_components/jsplumb/dist/js/jquery.jsPlumb-1.5.2.js',
+      'app/bower_components/underscore/underscore.js',
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      'test/spec/**/*.js',
     ],
 
     // list of files / patterns to exclude
@@ -31,7 +36,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
