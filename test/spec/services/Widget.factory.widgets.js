5c41a3c0668bdb5a25f40fa8931df93f55431896
Jon <srndpty@gmail.com>
2013-09-09 12:40:25 -0700

    fixed widget test

--- test/spec/services/Widget.factory.widgets.js
+++ test/spec/services/Widget.factory.widgets.js
@@ -1,37 +1,23 @@
 describe("Widget Service", function() {
 
-	var widgets;
+	var widget;
  	beforeEach(function() {
  		module('Puts');
 		module('WidgetServicer');
 		inject(function($injector){
 			var puts = $injector.get('Puts');
-			widgets = $injector.get('widgets',puts);
+			widget = $injector.get('widget');
 			}
 		);
 	});
 
-	describe("widgets", function() {
-		it("should be an arrray", function() {
-		  expect(widgets.widgets instanceof Array).toBeTruthy();
-		});
+	it("should contain inputs, outputs, int_inputs, int_outputs arrays", function() {
+
+		expect(widget.inputs instanceof Array).toBeTruthy();			
+		expect(widget.outputs instanceof Array).toBeTruthy();			
+		expect(widget.internal.inputs instanceof Array).toBeTruthy();			
+		expect(widget.internal.outputs instanceof Array).toBeTruthy();			
+		expect(widget.internal.widgets instanceof Array).toBeTruthy();
 	});
 
-	describe("getNewwidget()",function(){
-
-		beforeEach(function() {
-		  var widget = widgets.getNewwidget();
-		});
-		it("should return an object", function() {
-		  expect(widget.getNewWidget() instanceof Object).toBeTruthy();
-		});
-
-		xit("should contain an inputs, outputs and internal inputs and outputs objects", function() {
-			expect(widget.inputs instanceof Object).toBeTruthy();		  
-			expect(widget.outputs instanceof Object).toBeTruthy();		  
-			expect(widget.int_inputs instanceof Object).toBeTruthy();		  
-			expect(widget.int_outputs instanceof Object).toBeTruthy();		  
-
-		});
-	})
 });
