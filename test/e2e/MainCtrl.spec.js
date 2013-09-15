5b50da11349b9e0073df1f0b65be354a8201ec0f
Jon <srndpty@gmail.com>
2013-09-14 18:57:57 -0700

    added minor e2e tests

--- test/e2e/MainCtrl.spec.js
+++ test/e2e/MainCtrl.spec.js
@@ -1,7 +1,18 @@
 describe("MainCtrl", function() {
-	it("should display a herobox ", function() {
-		browser().navigateTo('/')
-	  expect(element("div").count()).toEqual(1);
+
+	beforeEach(function() {
+		browser().navigateTo('/');
+		sleep(3);
+
+	});
+
+	it("should display inputs and outputs divs", function() {
+	  expect(element("#outputs").count()).toEqual(1);
+	  expect(element("#inputs").count()).toEqual(1);
+
 	});
   
+  	it('should display as many input labels as the are in MIDIAccess.enumerateInputs()',function () {
+  		expect(element(".midiinputlabel").count()).toEqual(9);
+  	})
 });
