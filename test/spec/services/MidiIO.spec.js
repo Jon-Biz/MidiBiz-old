9e648503a4f6b710a82bc7398c23bcd5ddb7beaa
Jon <srndpty@gmail.com>
2013-09-16 19:33:57 -0700

    updated note parsing tests

--- test/spec/services/MidiIO.spec.js
+++ test/spec/services/MidiIO.spec.js
@@ -29,34 +29,34 @@ var MidiService;
 	  	expect(MidiService.msgParse("test") instanceof Object).toBeTruthy({});
 	  });
 
-		xdescribe("when sent message CMD:NOTE_OFF CHAN:0 NOTE:64 VELOCITY:0 TIME:202845000", function() {
-	  	
-	  	var note = 'CMD:NOTE_OFF CHAN:0 NOTE:64 VELOCITY:0 TIME:202845000';
+		describe("when sent message CMD:NOTE_OFF CHAN:0 NOTE:64 VELOCITY:0 TIME:202845000", function() {
+		  	
+		  	var note = 'CMD:NOTE_OFF CHAN:0 NOTE:64 VELOCITY:0 TIME:202845000';
 
-	  	it("should return an object with a CMD value of NOTE_OFF ", function() {
-		  	var parseValue = MidiService.msgParse(note);
+		  	it("should return an object with a CMD value of NOTE_OFF ", function() {
+			  	var parseValue = MidiService.msgParse(note);
 
-	  		expect(parseValue.CMD).toEqual("NOTE_OFF");  
-	  	});
-	  	it("should return object with CHAN of 0", function() {
-			var parseValue = MidiService.msgParse(note);
-	  		expect(parseValue.CHAN).toEqual(0);
-	  	});
+		  		expect(parseValue.CMD).toEqual("NOTE_OFF");  
+		  	});
+		  	it("should return object with CHAN of 0", function() {
+				var parseValue = MidiService.msgParse(note);
+		  		expect(parseValue.CHAN).toEqual(0);
+		  	});
 
-	  	it("should return object with NOTE of 64", function() {
-			var parseValue = MidiService.msgParse(note);
-	  		expect(parseValue.NOTE).toEqual(64);
-	  	});
+		  	it("should return object with NOTE of 64", function() {
+				var parseValue = MidiService.msgParse(note);
+		  		expect(parseValue.NOTE).toEqual(64);
+		  	});
 
-	  	it("should return object with VELOCITY of 0", function() {
-			var parseValue = MidiService.msgParse(note);
-	  		expect(parseValue.VELOCITY).toEqual(0);
-	  	});
-	   
-	  });
+		  	it("should return object with VELOCITY of 0", function() {
+				var parseValue = MidiService.msgParse(note);
+		  		expect(parseValue.VELOCITY).toEqual(0);
+		  	});
+		   
+		});
 
 
-		xdescribe("when sent message 'CMD:NOTE_ON CHAN:0 NOTE:63 VELOCITY:1 TIME:856523000 ' ", function() {
+		describe("when sent message 'CMD:NOTE_ON CHAN:0 NOTE:63 VELOCITY:1 TIME:856523000 ' ", function() {
 		  	var note = 'CMD:NOTE_ON CHAN:0 NOTE:63 VELOCITY:1 TIME:856523000 ';
 
 		  	it("should return an object with a CMD value of NOTE_OFF ", function() {
@@ -86,7 +86,7 @@ var MidiService;
 			});
 	});
 
-	xdescribe("reparsetMidiCMD",function(){
+	describe("reparsetMidiCMD",function(){
 	
 		describe("when sent a note on CMD", function() {
 		 it("should return midiBridge.NOTE_ON", function() {
@@ -98,10 +98,30 @@ var MidiService;
 		describe("when sent a note CMD", function() {
 		 it("should return midimessage from it", function() {
 		 	var repackedMsg = MidiService.reparseMidiCMD(note_off.cmd);	
-		    expect(repackedMsg).toEqual(midiBridge.NOTE_ON);
+		    expect(repackedMsg).toEqual(midiBridge.NOTE_OFF);
 		  }); 
 		});
 
+		describe("when sent 'NOTE_ON'", function() {
+		  it("should return 0x90 ", function() {
+		    expect(MidiService.reparseMidiCMD('NOTE_ON')).toEqual(0x90);
+		  });
+		});
+
 	})
+
+	describe("msgReparse", function() {
+	  
+	  describe("when given param {CMD:'NOTE_ON',CHAN:1,NOTE:48,VELOCITY:100}", function() {
+
+	  	it("should return a object with value CMD an integer", function() {
+	  		var noteobj = {CMD:'NOTE_ON',CHAN:1,NOTE:48,VELOCITY:100}
+			expect(noteobj.CMD).toEqual('NOTE_ON');
+
+			var note =  MidiService.msgReparse(noteobj)
+			expect(note.CMD).toEqual(0x90);
+	  	});
+	  });
+	});
 
 });
