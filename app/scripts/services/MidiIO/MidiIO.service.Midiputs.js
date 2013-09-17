7e2d260f2f7d0a20a732c93906548b5f786d7637
Jon <srndpty@gmail.com>
2013-09-16 18:56:29 -0700

    fixed faulty reference

--- app/scripts/services/MidiIO/MidiIO.service.Midiputs.js
+++ app/scripts/services/MidiIO/MidiIO.service.Midiputs.js
@@ -6,7 +6,7 @@ angular.module('MidiIO')
 
 .service('Midiputs',function (PutService) {
 	var that = PutService.getPutService();
-	//this.IO = Puts.getNewPutsCollection();
+	that.IO = that.getNewPutsCollection();
 
 	that.msgParse = function (msg) {
 
@@ -17,7 +17,6 @@ angular.module('MidiIO')
 		for (var i = splitmsg.length - 1; i >= 0; i--) {
 			var midiparam = splitmsg[i].split(':');
 			parsedmsg[midiparam[0]] = midiparam[1];
-			console.log(midiparam[0]+' '+midiparam[1]);
 		}
 
 		if(parsedmsg.CHAN){ parsedmsg.CHAN = parseInt(parsedmsg.CHAN,10);}
@@ -29,77 +28,84 @@ angular.module('MidiIO')
 
 	};
 
-	this.msgReparse = function (noteobj) {
+	that.msgReparse = function (noteobj) {
 		noteobj.CMD = this.reparseMidiCMD(noteobj.CMD);
 		return noteobj;
 	};
 
-	this.reparseMidiCMD = function(command){
+	that.reparseMidiCMD = function(command){
 
 		var midicommand;
 
 		switch(command)
 			{
 		case 'NOTE_OFF':
-			midicommand = midiBridge.NOTE_OFF;
+			midicommand = 0x80;
 			break;
 		case 'NOTE_ON':
-			midicommand = midiBridge.NOTE_ON;
+			midicommand = 0x90;
 			break;
 		case 'POLY_PRESSURE':
-			midicommand = midiBridge.POLY_PRESSURE;
+			midicommand = 0xA0;
 			break;
 		case 'CONTROL_CHANGE':
-			midicommand = midiBridge.CONTROL_CHANGE;
+			midicommand = 0xB0;
 			break;
 		case 'PROGRAM_CHANGE':
-			midicommand = midiBridge.PROGRAM_CHANGE;
+			midicommand = 0xC0;
 			break;
 		case 'CHANNEL_PRESSURE':
-			midicommand = midiBridge.CHANNEL_PRESSURE;
+			midicommand = 0xD0;
 			break;
 		case 'PITCH_BEND':
-			midicommand = midiBridge.PITCH_BEND;
+			midicommand = 0xE0;
 			break;
 		case 'SYSTEM_EXCLUSIVE':
-			midicommand = midiBridge.SYSTEM_EXCLUSIVE;
+			midicommand = 0xF0;
 			break;
 		case 'MIDI_TIMECODE':
-			midicommand = midiBridge.MIDI_TIMECODE;
+			midicommand = 241;
 			break;
 		case 'SONG_POSITION':
-			midicommand = midiBridge.SONG_POSITION;
+			midicommand = 242;
 			break;
 		case 'SONG_SELECT':
-			midicommand = midiBridge.SONG_SELECT;
+			midicommand = 243;
 			break;
 		case 'TUNE_REQUEST':
-			midicommand = midiBridge.TUNE_REQUEST;
+			midicommand = 246;
 			break;
 		case 'EOX':
-			midicommand = midiBridge.EOX;
+			midicommand = 247;
 			break;
 		case 'TIMING_CLOCK':
-			midicommand = midiBridge.TIMING_CLOCK;
+			midicommand = 248;
 			break;
 		case 'START':
-			midicommand = midiBridge.START;
+			midicommand = 250;
 			break;
 		case 'CONTINUE':
-			midicommand = midiBridge.CONTINUE;
+			midicommand = 251;
 			break;
 		case 'STOP':
-			midicommand = midiBridge.STOP;
+			midicommand = 252;
 			break;
 		case 'ACTIVE_SENSING':
-			midicommand = midiBridge.ACTIVE_SENSING;
+			midicommand = 254;
 			break;
 		case 'SYSTEM_RESET':
-			midicommand = midiBridge.SYSTEM_RESET;
+			midicommand = 255;
 			break;
 		}
+        
+  //       //other statics
+  //       NOTE_NAMES_SHARP : "sharp",
+  //       NOTE_NAMES_FLAT : "flat",
+  //       NOTE_NAMES_SOUNDFONT : "soundfont",
+  //       NOTE_NAMES_ENHARMONIC_SHARP : "enh-sharp",
+  //       NOTE_NAMES_ENHARMONIC_FLAT : "enh-flat",
+        return midicommand;
 
-		return midicommand;
 	};
 
 	return that;
